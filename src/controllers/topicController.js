9592a7570305b263dd0d5046beec5d89b7338741
Kamil <kamilbednarz91@gmail.com>
2019-07-16 21:37:23 -0500

    The Topic Resource: Part 2

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -1,10 +1,11 @@
 const topicQueries = require("../db/queries.topics.js");
 
 module.exports = {
+
   index(req, res, next){
 
     topicQueries.getAllTopics((err, topics) => {
-      if(err){
+        if(err){
         res.redirect(500, "static/index");
       } else {
         res.render("topics/index", {topics});
