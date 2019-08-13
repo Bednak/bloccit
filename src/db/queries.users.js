f4effdc264cd6656316a16d10c2d9e2bd7dc72c0
Kamil <kamilbednarz91@gmail.com>
2019-08-13 12:48:37 -0500

    Checkpoint-3008-Profile

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,7 +1,10 @@
 const User = require("./models").User;
+const Post = require("./models").Post;
+const Comment = require("./models").Comment;
 const bcrypt = require("bcryptjs");
 
 module.exports = {
+
   createUser(newUser, callback){
 
     const salt = bcrypt.genSaltSync();
@@ -16,6 +19,37 @@ module.exports = {
     })
     .catch((err) => {
       callback(err);
+    })
+  },
+
+  getUser(id, callback){
+
+    let result = {};
+    User.findById(id)
+    .then((user) => {
+
+      if(!user) {
+        callback(404);
+      } else {
+
+        result["user"] = user;
+
+        Post.scope({method: ["lastFiveFor", id]}).findAll()
+        .then((posts) => {
+
+          result["posts"] = posts;
+
+          Comment.scope({method: ["lastFiveFor", id]}).findAll()
+          .then((comments) => {
+
+            result["comments"] = comments;
+            callback(null, result);
+          })
+          .catch((err) => {
+            callback(err);
+          })
+        })
+      }
     })
   }
 
